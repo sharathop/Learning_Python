cfb3463e64ceb558a6b1d104c98eb505dddba756
Sharathop <sharathparajji@gmail.com>
2026-03-03 11:24:04 +0530

    updated with json objects

--- Bank_account.py
+++ Bank_account.py
@@ -7,6 +7,19 @@ class Bankaccount:
         self.age = age
         self.balance = balance
 
+    def to_dict(self):
+        return{ 
+            "bank_details":{
+            "bank_name" : self.bank_name,
+            "cname" : self.cname,
+            "acc" : self.acc,
+            "age" : self.age,
+            "balance":self.balance
+
+            }
+
+        }
+
     def detail(self):
         print(f"CNAme:{self.cname}")
         print(f"acc:{self.acc}")
@@ -33,6 +46,13 @@ class saving_account(Bankaccount):
             self.balance -= withd + self.fee
             print(f"after withdraw:{self.balance}")
 
+    def to_dict(self):
+        data=super().to_dict()
+        data['bank_details']['fee'] = self.fee
+        data['bank_details']['account_type'] = "saving"
+        return data
+
+
 acc1 = Bankaccount('Sharath',123456,22,5000)
 
 acc1.detail()
@@ -44,3 +64,13 @@ sav1 =saving_account('vk',465415,21,6000,40)
 
 sav1.detail()
 sav1.withdraw(50000)
+
+import json
+
+json_data = json.dumps([acc1.to_dict(),sav1.to_dict()], indent=4)
+
+print(json_data)
+
+
+with open('bank.json', 'w') as f:
+    json.dump([acc1.to_dict(),sav1.to_dict()], f, indent=4)
